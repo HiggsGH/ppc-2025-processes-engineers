e2b17863dcd8ddbad6fb3f6b8ee51d8c757dfcbc
HiggsGH <ilya.batushin@yandex.ru>
2025-11-20 12:30:10 +0300

    Fix tests name

--- tasks/batushin_i_max_val_rows_matrix/tests/functional/main.cpp
+++ tasks/batushin_i_max_val_rows_matrix/tests/functional/main.cpp
@@ -20,7 +20,7 @@
 
 namespace batushin_i_max_val_rows_matrix {
 
-class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
+class BatushinIMaxValRowsMatrixFuncTests : public ppc::util::BaseRunFuncTests<InType, OutType, TestType> {
  public:
   static std::string PrintTestParam(const TestType &test_param) {
     return std::to_string(std::get<0>(test_param)) + "_" + std::get<1>(test_param);
@@ -65,7 +65,7 @@ class NesterovARunFuncTestsProcesses : public ppc::util::BaseRunFuncTests<InType
 
 namespace {
 
-TEST_P(NesterovARunFuncTestsProcesses, MatmulFromPic) {
+TEST_P(BatushinIMaxValRowsMatrixFuncTests, MatmulFromPic) {
   ExecuteTest(GetParam());
 }
 
@@ -77,9 +77,9 @@ const auto kTestTasksList =
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);
 
-const auto kPerfTestName = NesterovARunFuncTestsProcesses::PrintFuncTestName<NesterovARunFuncTestsProcesses>;
+const auto kPerfTestName = BatushinIMaxValRowsMatrixFuncTests::PrintFuncTestName<BatushinIMaxValRowsMatrixFuncTests>;
 
-INSTANTIATE_TEST_SUITE_P(PicMatrixTests, NesterovARunFuncTestsProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(PicMatrixTests, BatushinIMaxValRowsMatrixFuncTests, kGtestValues, kPerfTestName);
 
 }  // namespace
 
--- tasks/batushin_i_max_val_rows_matrix/tests/performance/main.cpp
+++ tasks/batushin_i_max_val_rows_matrix/tests/performance/main.cpp
@@ -7,7 +7,7 @@
 
 namespace batushin_i_max_val_rows_matrix {
 
-class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, OutType> {
+class BatushinIMaxValRowsMatrixPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
   const int kCount_ = 100;
   InType input_data_{};
 
@@ -24,7 +24,7 @@ class ExampleRunPerfTestProcesses : public ppc::util::BaseRunPerfTests<InType, O
   }
 };
 
-TEST_P(ExampleRunPerfTestProcesses, RunPerfModes) {
+TEST_P(BatushinIMaxValRowsMatrixPerfTests, RunPerfModes) {
   ExecuteTest(GetParam());
 }
 
@@ -33,8 +33,8 @@ const auto kAllPerfTasks =
 
 const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
 
-const auto kPerfTestName = ExampleRunPerfTestProcesses::CustomPerfTestName;
+const auto kPerfTestName = BatushinIMaxValRowsMatrixPerfTests::CustomPerfTestName;
 
-INSTANTIATE_TEST_SUITE_P(RunModeTests, ExampleRunPerfTestProcesses, kGtestValues, kPerfTestName);
+INSTANTIATE_TEST_SUITE_P(RunModeTests, BatushinIMaxValRowsMatrixPerfTests, kGtestValues, kPerfTestName);
 
 }  // namespace batushin_i_max_val_rows_matrix

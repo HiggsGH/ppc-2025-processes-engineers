21d898cd56de5c6e68244ba5cd7fe04d1ce65c7d
Ilya Batushin <ilya.batushin@yandex.ru>
2025-12-18 18:55:20 +0300

    clang-format :)

--- tasks/batushin_i_striped_matrix_multiplication/mpi/src/ops_mpi.cpp
+++ tasks/batushin_i_striped_matrix_multiplication/mpi/src/ops_mpi.cpp
@@ -52,9 +52,7 @@ bool BatushinIStripedMatrixMultiplicationMPI::PreProcessingImpl() {
 
 namespace {
 
-enum class MPITag : std::uint8_t {
-    kMatrixB = 101
-};
+enum class MPITag : std::uint8_t { kMatrixB = 101 };
 
 std::vector<int> ComputeBlockSizes(int total, int num_procs) {
   std::vector<int> sizes(num_procs, 0);
